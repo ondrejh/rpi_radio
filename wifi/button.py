c66a23df9a1292771b6df851df81c39e2be7d856
Ondrej Hejda <ondrej.hejda@saviotechnics.com>
2019-05-02 14:53:04 +0200

    button polling thread with flask test interface (if not run on raspberry)

--- wifi/button.py
+++ wifi/button.py
@@ -1,29 +1,94 @@
 #!/usr/bin/python3
 
-import RPi.GPIO as GPIO
 from time import sleep
+import threading
+test = False
+
+try:
+	import RPi.GPIO as GPIO
+
+except ImportError:
+	test = True
+	print("Can't import RPi.GPIO .. run test.")
+
+	from flask import Flask, render_template, url_for
+	app = Flask(__name__)
+
+	btns = []
+
+	@app.route('/')
+	@app.route('/<name>/')
+	def hello(name=None):
+		if name in ('1', '2'):
+			btns.append(int(name))
+		return render_template('temp.html', name=name)
+
 
 def btn1():
-	return (GPIO.input(20) == 0)
+	if not test:
+		return GPIO.input(20) == 0
+	else:
+		if 1 in btns:
+			btns.remove(1)
+			return True
+	return False
+
 
 def btn2():
-	return (GPIO.input(21) == 0)
+	if not test:
+		return GPIO.input(21) == 0
+	else:
+		if 2 in btns:
+			btns.remove(2)
+			return True
+	return False
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(17, GPIO.OUT)
 
-btn1s = btn2s = False
+class ButtonsThread(threading.Thread):
 
-while True:
-	tmp1 = btn1()
-	tmp2 = btn2()
-	if tmp1 != btn1s:
-		print('Button 1 {}'.format('PRESSED' if tmp1 else 'RELEASED'))
-		btn1s = tmp1
-	if tmp2 != btn2s:
-		print('Button 2 {}'.format('PRESSED' if tmp2 else 'RELEASED'))
-		btn2s = tmp2
-	sleep(0.1)
+	def __init__(self):
+
+		threading.Thread.__init__(self)
+		self.stop = False
+
+		self.btn1s = False
+		self.btn2s = False
+
+	def stop(self):
+		self.stop = True
+
+	def run(self):
+
+		if not test:
+			GPIO.setmode(GPIO.BCM)
+			GPIO.setwarnings(False)
+			GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+			GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+			GPIO.setup(17, GPIO.OUT)
+
+		while not self.stop:
+			tmp = btn1()
+			if tmp != self.btn1s:
+				if tmp:
+					print('Button 1 PRESSED')
+				self.btn1s = tmp
+			tmp = btn2()
+			if tmp != self.btn2s:
+				if tmp:
+					print('Button 2 PRESSED')
+				self.btn2s = tmp
+			sleep(0.1)
+
+
+if __name__ == '__main__':
+
+	btnThread = ButtonsThread()
+	btnThread.start()
+
+	if test:
+		app.run()
+
+	while True:
+		sleep(1)
+
+	print('Finito !!!')
